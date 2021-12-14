4b64d20de1a64357be2e0eb76e1752e02183bfd0
vaibhawj <vj.vaibhaw@gmail.com>
2021-12-15 01:33:02 +0530

    Day12 part1

--- day12/day12.go
+++ day12/day12.go
@@ -1,1 +1,67 @@
 package day12
+
+import (
+	"fmt"
+	"strings"
+)
+
+func PossiblePaths(input string) int {
+	caveMap := make(map[string][]string)
+	for _, line := range strings.Split(input, "\n") {
+		c1 := strings.Split(line, "-")[0]
+		c2 := strings.Split(line, "-")[1]
+		caveMap[c1] = append(caveMap[c1], c2)
+		caveMap[c2] = append(caveMap[c2], c1)
+	}
+	fmt.Println(caveMap)
+
+	var paths []string
+	var traversal []string
+	traverse("start", traversal, caveMap, &paths)
+	fmt.Println()
+	fmt.Println(paths)
+
+	return len(paths)
+}
+
+func traverse(cave string, traversal []string, caveMap map[string][]string, paths *[]string) {
+	traversal = append(traversal, cave)
+	if cave == "end" {
+		traversalStr := strings.Join(traversal, ",")
+		if !alreadyContainedInPaths(traversalStr, paths) {
+			*paths = append(*paths, traversalStr)
+		}
+	} else {
+		for _, c := range caveMap[cave] {
+			if c == "start" {
+				continue
+			}
+			if strings.ToLower(c) == c && alreadyContainedInTraversal(c, traversal) {
+				continue
+			}
+			traverse(c, traversal, caveMap, paths)
+		}
+	}
+}
+
+func alreadyContainedInPaths(traversal string, paths *[]string) bool {
+	for _, p := range *paths {
+		if p == traversal {
+			return true
+		}
+	}
+	return false
+}
+
+func alreadyContainedInTraversal(cave string, traversal []string) bool {
+	for _, c := range traversal {
+		if c == cave {
+			return true
+		}
+	}
+	return false
+}
+
+func isLowerCase(s string) bool {
+	return strings.ToLower(s) == s
+}
--- day12/day12_test.go
+++ day12/day12_test.go
@@ -1,1 +1,35 @@
 package day12
+
+import "testing"
+
+func TestPossiblePaths(t *testing.T) {
+	actual := PossiblePaths("start-A\nstart-b\nA-c\nA-b\nb-d\nA-end\nb-end")
+	expected := 10
+	if actual != expected {
+		t.Errorf("got %d, expected %d", actual, expected)
+	}
+}
+
+func TestPossiblePaths2(t *testing.T) {
+	actual := PossiblePaths("dc-end\nHN-start\nstart-kj\ndc-start\ndc-HN\nLN-dc\nHN-end\nkj-sa\nkj-HN\nkj-dcdc-end\nHN-start\nstart-kj\ndc-start\ndc-HN\nLN-dc\nHN-end\nkj-sa\nkj-HN\nkj-dc")
+	expected := 19
+	if actual != expected {
+		t.Errorf("got %d, expected %d", actual, expected)
+	}
+}
+
+func TestPossiblePaths3(t *testing.T) {
+	actual := PossiblePaths("fs-end\nhe-DX\nfs-he\nstart-DX\npj-DX\nend-zg\nzg-sl\nzg-pj\npj-he\nRW-he\nfs-DX\npj-RW\nzg-RW\nstart-pj\nhe-WI\nzg-he\npj-fs\nstart-RW")
+	expected := 226
+	if actual != expected {
+		t.Errorf("got %d, expected %d", actual, expected)
+	}
+}
+
+func TestPossiblePaths_Real(t *testing.T) {
+	actual := PossiblePaths("HF-qu\nend-CF\nCF-ae\nvi-HF\nvt-HF\nqu-CF\nhu-vt\nCF-pk\nCF-vi\nqu-ae\nae-hu\nHF-start\nvt-end\nae-HF\nend-vi\nvi-vt\nhu-start\nstart-ae\nCS-hu\nCF-vt")
+	expected := 3802
+	if actual != expected {
+		t.Errorf("got %d, expected %d", actual, expected)
+	}
+}
